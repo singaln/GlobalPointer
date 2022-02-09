059efa48e3aacb44c662300c9468446226e7311c
bin.w <1105193825@qq.com>
2022-02-09 10:56:32 +0800

    update

--- utils.py
+++ utils.py
@@ -87,7 +87,7 @@ def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    if not args.no_cuda and torch.cuda.is_available():
+    if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
 
 
